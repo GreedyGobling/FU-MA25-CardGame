164844dd2ae52b55fd1d99c5cb6eac4bddcbd515
GreedyGobling <greedygobling@gmail.com>
2025-11-23 14:00:02 +0100

    todo and fragment

--- app/src/main/java/com/example/cardgame/MainActivity.kt
+++ app/src/main/java/com/example/cardgame/MainActivity.kt
@@ -3,8 +3,6 @@ package com.example.cardgame
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 import com.example.cardgame.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -16,7 +14,20 @@ class MainActivity : AppCompatActivity() {
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
 
+        binding.startmenu.setOnClickListener {
+            // TODO
+            // if active game is true show 2 button. continy and new game
+            // if active game is false start new game
+        }
 
 
+    }
+
+
+
+    fun addStartMenu(){
+        val startMenu = Gamemenu()
+        val transaction = supportFragmentManager.beginTransaction()
+        transaction.add(R.id.container, startMenu, startMenu)
     }
 }
