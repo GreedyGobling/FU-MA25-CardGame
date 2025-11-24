b8afd38ce7304b6780d4c73c9ce28d19e981a987
GreedyGobling <greedygobling@gmail.com>
2025-11-24 11:42:57 +0100

    update card for suit symbol and rank symbol "ace don't have a 1"

--- app/src/main/java/com/example/cardgame/Card.kt
+++ app/src/main/java/com/example/cardgame/Card.kt
@@ -1,28 +1,37 @@
 package com.example.cardgame
 
 data class Card(
-    val suit: suit,
+    val suit: Suit,
     val rank: Rank
-)
+){
+    fun getSuitSymbol(): String{
+        return when (suit) {
+            Suit.Heart -> "♥"
+            Suit.Diamond -> "♦"
+            Suit.Clubs -> "♣"
+            Suit.Spades -> "♠"
+        }
+    }
+}
 
-enum class suit{
+enum class Suit{
     Heart, Spades, Clubs, Diamond
 }
 
 // TODO enum for the ranks
 
-enum class Rank(val value: Int){
-    ACE(1),
-    TWO(2),
-    THREE(3),
-    FOUR(4),
-    FIVE(5),
-    SIX(6),
-    SEVEN(7),
-    EIGHT(8),
-    NINE(9),
-    TEN(10),
-    JACK(11),
-    QUEEN(12),
-    KING(13)
+enum class Rank(val value: Int val symbol: String){
+    ACE(1, "A"),
+    TWO(2, "2"),
+    THREE(3, "3"),
+    FOUR(4, "4"),
+    FIVE(5, "5"),
+    SIX(6, "6"),
+    SEVEN(7, "7"),
+    EIGHT(8, "8"),
+    NINE(9, "9"),
+    TEN(10, "10"),
+    JACK(11, "J"),
+    QUEEN(12, "Q"),
+    KING(13, "K")
 }
