b883ccc3205006d39b118d2fc63a01326caeb345
GreedyGobling <greedygobling@gmail.com>
2025-11-26 20:52:57 +0100

    started working on GameActivity

--- app/src/main/java/com/example/cardgame/GameActivity.kt
+++ app/src/main/java/com/example/cardgame/GameActivity.kt
@@ -5,16 +5,38 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.cardgame.databinding.ActivityGameBinding
 
 class GameActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityGameBinding
+
+    private val game = BlackJack()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_game)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        binding = ActivityGameBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        // Buttons hit and stand
+    }
+
+    private fun onHitClicked(){
+        game.playerHit()
+        //TODO update for hand draw
+        if (game.getPlayerHand().isBust()) {
+            endGame()
         }
     }
+    private fun onStandClicked(){
+        game.playerStand()
+        // update dealerhand if needed
+        endGame()
+    }
+
+    private fun endGame(){
+        val result = game.determineWinner()
+    }
+
+    //TODO create Card for win/lose
 }
