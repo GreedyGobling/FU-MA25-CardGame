de173f06d19dd8709e4a2925263f8b050a1dd579
GreedyGobling <greedygobling@gmail.com>
2025-12-01 15:11:16 +0100

    changed plans to only sharedpref

--- app/src/main/java/com/example/cardgame/GameStats.kt
+++ app/src/main/java/com/example/cardgame/GameStats.kt
@@ -1,14 +1,11 @@
 package com.example.cardgame
 
-data class Stats(
-    var gamesPlayed: Int = 0,
-    var gameWon: Int = 0,
-    var gameLost: Int = 0,
-    var blackjacks: Int = 0,
-    var currentStreak: Int = 0,
-    var bestStreak: Int = 0,
-)
-
 object GameStats {
-
+    private const val PREFS_NAME = "blackjack_stats"
+    private const val KEY_GAMES_PLAYED = "games_played"
+    private const val KEY_GAMES_WON = "games_won"
+    private const val KEY_GAMES_LOST = "games_lost"
+    private const val KEY_BLACKJACKS = "blackjacks"
+    private const val KEY_CURRENT_STREAK = "current_streak"
+    private const val KEY_BEST_STREAK = "best_streak"
 }
