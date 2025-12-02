4719c7437225db38e705dacd445f215fe2e6a3e5
GreedyGobling <greedygobling@gmail.com>
2025-12-02 15:08:31 +0100

    update stats done

--- app/src/main/java/com/example/cardgame/GameStats.kt
+++ app/src/main/java/com/example/cardgame/GameStats.kt
@@ -19,6 +19,27 @@ object GameStats {
         val gamesPlayed = prefs.getInt(KEY_GAMES_PLAYED, 0)
         editor.putInt(KEY_GAMES_PLAYED, gamesPlayed + 1)
 
+        val playerWon = when (result) {
+            GameResult.PLAYERWIN, GameResult.PLAYEBLACKJACK, GameResult.DEALERBUST -> true
+            else -> false
+        }
+
+        if (playerWon) {
+            val gamesWon = prefs.getInt(KEY_GAMES_WON, 0)
+            editor.putInt(KEY_GAMES_WON, gamesWon + 1)
+
+            val currentStreak = prefs.getInt(KEY_CURRENT_STREAK, 0) + 1
+            editor.putInt(KEY_CURRENT_STREAK, currentStreak)
+
+            val bestStreak = prefs.getInt(KEY_BEST_STREAK, 0)
+            if (currentStreak > bestStreak) {
+                editor.putInt(KEY_BEST_STREAK, currentStreak)
+            }
+        } else {
+            val gamesLost = prefs.getInt(KEY_GAMES_LOST, 0)
+            editor.putInt(KEY_GAMES_LOST, gamesLost + 1)
+            editor.putInt(KEY_CURRENT_STREAK, 0)
+        }
         editor.apply()
     }
 
